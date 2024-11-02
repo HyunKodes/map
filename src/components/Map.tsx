ccc850d81facd802090c2759d456e38022558b9e
Tony Kwak <tkdev.to@gmail.com>
2024-11-02 15:38:24 -0400

    changed location to Toronto

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -20,7 +20,7 @@ export default function map({ place }: MapProps) {
   return (
     <MapContainer
       ref={mapRef}
-      center={[40.7, -74]}
+      center={[43.6, -79.4]}
       zoom={12}
       scrollWheelZoom
       className="h-full"
